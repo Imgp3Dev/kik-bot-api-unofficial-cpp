c739f9b2454ecbcaf66bee43e062e500dc0cdfaf
SunTzu <arithmetic@gmail.com>
2018-12-28 06:25:03 -0600

    Stage2

---
+++ stage2/Stage2.cpp
@@ -0,0 +1,36 @@
+#include "Client.cpp"
+#include <CImg.h>
+#include <gloox/messageHandler.h>
+#include <gloox/client.h>
+using namespace gloox;
+
+class Message : public MessageSessionHandler, KikClient 
+{
+ Message() : m_session(jid)
+ virtual ~Message()
+ void start(){
+ JID jid("yourjid@talk.kik.com")
+/*Feel free to edit that above. but what it does is rely only on your jid for commands*/
+KikClient : MessageListener(jid, "Image", image);
+Response()-> setServer()-> MessageHandler(){
+client;
+/*Say "Image" and it will output an image that you added in CImg code you can replace with URL or image extension*/
+ }
+/*Pull Jids(if you don't want this functionality then delete the code*/
+KikClient :: Message {
+MessageListener(jid "jid");
+client->RosterManager(this, "jid", "");
+}
+/*You put "jid <username>" and it will rely on the RoasterManger for getting the user jid*/
+
+int main(){
+	CIMg :: <unsigned char> image("Image.jpg"),visu(500, 400, 1, 3, 0);
+CImgDisplay main_disp(image);
+KikClient :: RosterManager(){
+parse->image();
+}
+//Using CImg to output images
+KikClient client;
+} 
+ };
+
